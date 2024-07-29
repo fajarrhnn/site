6790bde5c5bb4eb5d6f29c090a5586a83d6c88d9
fajarrhnn <fajarrehan023@gmail.com>
2024-07-29 13:22:29 +0700

    create contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -16,19 +16,35 @@ export default function Contact() {
     message: "",
   });
 
-  const submitContact: FormEventHandler<HTMLFormElement> = (e) => {
+  const submitContact: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    console.log(JSON.stringify(data));
     toast({
       title: "Berhasil Mengirimkan Pesan",
+      description: `${data.name} - ${data.email} - ${data.subject} - ${data.message}`,
     });
+  };
+
+  const changeValue: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = (e) => {
+    const { name, value } = e.target;
+
+    setData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
 
   return (
     <>
-      <section className="grid gap-4 w-full max-w-3xl mx-auto">
+      <section className="w-full mx-auto flex flex-col space-y-5 h-screen items-center justify-center">
         <h1 className="text-center text-5xl">Contact Me</h1>
-        <form className="grid gap-4" onSubmit={submitContact}>
+        <form
+          className="grid gap-4 w-full max-w-xl"
+          method="POST"
+          onSubmit={submitContact}
+          // action="mailto:fajarrehan023@gmail.com"
+        >
           <div className="w-full max-w-full mx-auto flex items-center space-x-2">
             <div className="w-1/2 mx-auto flex flex-col space-y-3">
               <Label htmlFor="name">Name</Label>
@@ -37,9 +53,7 @@ export default function Contact() {
                 id="name"
                 name="name"
                 value={data.name}
-                onChange={(e) =>
-                  setData((prev) => ({ ...prev, name: e.target.value }))
-                }
+                onChange={changeValue}
               />
             </div>
             <div className="w-1/2 mx-auto flex flex-col space-y-3">
@@ -49,9 +63,7 @@ export default function Contact() {
                 id="email"
                 name="email"
                 value={data.email}
-                onChange={(e) => {
-                  setData((prev) => ({ ...prev, email: e.target.value }));
-                }}
+                onChange={changeValue}
               />
             </div>
           </div>
@@ -62,9 +74,7 @@ export default function Contact() {
               id="subject"
               name="subject"
               value={data.subject}
-              onChange={(e) => {
-                setData((prev) => ({ ...prev, subject: e.target.value }));
-              }}
+              onChange={changeValue}
             />
           </div>
           <div className="flex flex-col space-y-3">
@@ -73,9 +83,7 @@ export default function Contact() {
               id="message"
               name="message"
               value={data.message}
-              onChange={(e) => {
-                setData((prev) => ({ ...prev, message: e.target.value }));
-              }}
+              onChange={changeValue}
             />
           </div>
           <Button size={"lg"} type="submit">
