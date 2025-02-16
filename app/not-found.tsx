4a2c4fc8898016317def37d30a4c368babe73a0b
fajarrhnn <fajarrehan023@gmail.com>
2025-02-16 21:20:18 +0700

    updated

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,8 @@
 "use client";
 import { RefreshCcw } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/router";
 
 export default function NotFound({ message }: { message: string }) {
-  const { reload } = useRouter();
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="text-center">
@@ -17,7 +15,11 @@ export default function NotFound({ message }: { message: string }) {
         <p className="text-xl text-gray-500 dark:text-gray-500 mb-8">
           Silahkan buat kutipan untuk pertama kalinya!
         </p>
-        <Button onClick={() => reload()} variant={"outline"} size={"lg"}>
+        <Button
+          onClick={() => window.location.reload()}
+          variant={"outline"}
+          size={"lg"}
+        >
           <RefreshCcw />
           Try again
         </Button>
